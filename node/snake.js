4cf09c1ea720a6daa42fe43edc65d458719e9e30
woosungchu <woosungchu@gmail.com>
2017-12-05 18:19:09 +0900

    .

---
+++ node/snake.js
@@ -0,0 +1,118 @@
+window.onload = function WindowLoad(event) {
+	main();
+}
+
+var UP=0, DOWN=1, LEFT=2, RIGHT=3, SCALE=10; 
+
+function main(){
+	var container = document.getElementById('container');
+	
+	snake = new Snake(container);
+		
+}
+
+Snake = function(container){
+	this.snake = null,
+	this.timer = new Timer(20),
+	this.snakeParts = [],
+	this.ticks =0,
+	this.direction = DOWN,
+	this.score,
+	this.tailLength = 10,
+	this.time = 0,
+	this.over = false,
+	this.paused = false;
+	
+	this.init = function(container){
+		container.style.width = '800px';
+		container.style.height = '700px';
+		container.style.margin = '0 auto';
+		
+		this.over=false;
+		this.score=0;
+		this.tailLength=1;
+		this.ticks=0;
+		this.time=0;
+		this.direction=DOWN;
+		
+		this.startGame();
+		/*
+		dim=Toolkit.getDefaultToolkit().getScreenSize();
+		jframe=new JFrame("Snake");
+		jframe.setVisible(true);
+		jframe.setSize(800,700);
+		jframe.setResizable(false);
+		jframe.setLocation(dim.width/2-jframe.getWidth()/2, dim.height/2 - jframe.getHeight()/2);
+		jframe.add(renderPanel=new RenderPanel());
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.addKeyListener(this);
+		startGame();
+		*/
+	}
+	
+	this.startGame = function(){
+		
+		head= new Point(0,-1);
+		x = getRandomInt(0,78);
+		y = getRandomInt(0,66);
+		snakeParts = [];
+		cherry= new Point(x, y);
+		
+		for (var i = 0; i < this.tailLength; i++) {
+			snakeParts.push(new Point(head.x,head.y));
+		}
+		this.timer.start();
+	}
+		
+		
+	
+	/*
+	 
+    public static Snake snake; 
+	public JFrame jframe;
+	public RenderPanel renderPanel;
+	public Timer timer=new Timer(20,this);
+	public ArrayList<Point> snakeParts=new ArrayList<Point>();
+	public static int UP=0, DOWN=1, LEFT=2, RIGHT=3, SCALE=10;
+	public int ticks=0, direction=DOWN, score, tailLength=10,time=0;
+	public Point head, cherry;
+	public Random random;
+	public boolean over=false, paused ;
+	public Dimension dim;
+	  
+	  
+	 */
+	
+	this.init(container);
+}
+
+Point = function(x,y){
+	this.x =x;
+	this.y =y;
+}
+
+Timer = function(speed){
+	this.time = 0;
+	this.speed= speed;
+	
+	this.start = function(){
+		var inc = 10 * this.speed;
+		
+		setInterval(function(t){
+			console.log(t)
+			increment(t)
+		},inc);
+		
+		function increment(val){
+			this.time += val;
+		}
+	}
+	
+	this.getTimer = function(){
+		return this.time;
+	}
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
